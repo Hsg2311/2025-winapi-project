781f9bbe5c689c3bf2021571c482358356efcd5f
orange2458 <asm1133@naver.com>
2025-05-24 14:39:53 +0900

    프로젝트 초기화

---
+++ HeadSoccer/HeadSoccer/include/common.hpp
@@ -0,0 +1,17 @@
+#ifndef __common_hpp
+#define __common_hpp
+
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <algorithm>
+
+namespace Gdiplus {
+	using std::min;
+	using std::max;
+}
+
+#include <atlimage.h>
+
+#endif	// __common_hpp
