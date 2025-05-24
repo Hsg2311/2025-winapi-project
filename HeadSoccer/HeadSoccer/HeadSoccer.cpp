47490a15c98ada5dc7b4b301f5077c3a027d452c
orange2458 <asm1133@naver.com>
2025-05-24 15:24:32 +0900

    PeekMessage 방식으로 변경

--- HeadSoccer/HeadSoccer/HeadSoccer.cpp
+++ HeadSoccer/HeadSoccer/HeadSoccer.cpp
@@ -3,10 +3,9 @@
 
 #define MAX_LOADSTRING 100
 
-// 전역 변수:
-HINSTANCE hInst;                                // 현재 인스턴스입니다.
-WCHAR szTitle[ MAX_LOADSTRING ];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[ MAX_LOADSTRING ];            // 기본 창 클래스 이름입니다.
+HINSTANCE hInst;
+WCHAR szTitle[ MAX_LOADSTRING ];
+WCHAR szWindowClass[ MAX_LOADSTRING ];
 HWND gHwnd;
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
@@ -40,13 +39,20 @@ int APIENTRY wWinMain( _In_ HINSTANCE hInstance,
 
 	MSG msg;
 
-	// 기본 메시지 루프입니다:
-	while ( GetMessage( &msg, nullptr, 0, 0 ) )
-	{
-		if ( !TranslateAccelerator( msg.hwnd, hAccelTable, &msg ) )
-		{
-			TranslateMessage( &msg );
-			DispatchMessage( &msg );
+	while ( true ) {
+		if ( PeekMessage( &msg, nullptr, 0, 0, PM_REMOVE ) ) {
+			if ( msg.message == WM_QUIT ) {
+				break;
+			}
+
+			if ( !TranslateAccelerator( msg.hwnd, hAccelTable, &msg ) )
+			{
+				TranslateMessage( &msg );
+				DispatchMessage( &msg );
+			}
+		}
+		else {
+			
 		}
 	}
 
