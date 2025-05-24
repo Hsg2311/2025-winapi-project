d8c25472965d6b7c7e1ea238cca23a3ac6fa046f
orange2458 <asm1133@naver.com>
2025-05-24 15:33:34 +0900

    menu 사용 안함

--- HeadSoccer/HeadSoccer/HeadSoccer.cpp
+++ HeadSoccer/HeadSoccer/HeadSoccer.cpp
@@ -80,7 +80,7 @@ ATOM MyRegisterClass( HINSTANCE hInstance )
 	wcex.hIcon = LoadIcon( hInstance, MAKEINTRESOURCE( IDI_HEADSOCCER ) );
 	wcex.hCursor = LoadCursor( nullptr, IDC_ARROW );
 	wcex.hbrBackground = (HBRUSH)( COLOR_WINDOW + 1 );
-	wcex.lpszMenuName = MAKEINTRESOURCEW( IDC_HEADSOCCER );
+	wcex.lpszMenuName = nullptr;	//MAKEINTRESOURCEW( IDC_HEADSOCCER );
 	wcex.lpszClassName = szWindowClass;
 	wcex.hIconSm = LoadIcon( wcex.hInstance, MAKEINTRESOURCE( IDI_SMALL ) );
 
