4a55f8f90d80c74172c04b0a9914dc15c570a41c
25-0308 <00rhkrtmdwns@hanmail.net>
2025-06-07 14:04:21 +0900

    플레이어 이동

--- HeadSoccer/HeadSoccer/include/Player.hpp
+++ HeadSoccer/HeadSoccer/include/Player.hpp
@@ -4,8 +4,18 @@ class Player : public Object
 {
 public:
 	int pnum_;
+
 	void init();
 	void move();
+
+	void pspeed() {
+		speed += 0.1f;
+	}
+
+	void pspeed() {
+		if (speed > 0.1f) speed -= 0.1f;
+	}
+
 private:
 	float speed;
 };
--- HeadSoccer/HeadSoccer/src/Player.cpp
+++ HeadSoccer/HeadSoccer/src/Player.cpp
@@ -4,34 +4,33 @@
 
 void Player::move() {
     float dt = Timer::fdt();
-    const auto& inputInfo = InputHandler::getInputInfo();
 
-    float dx = 0.0f, dy = 0.0f;
-
-    if (inputInfo[etoi(Key::Up)].state == KeyState::Tap ||
-        inputInfo[etoi(Key::Up)].state == KeyState::Hold) {
-        dy -= speed; // 위로 이동
+    if (pnum_ == 1) {
+        if (InputHandler::getKeyState(Key::W) == KeyState::Tap ||
+            InputHandler::getKeyState(Key::W) == KeyState::Hold) {
+            pos_.y -= speed * dt; // 점프
+        }
+        if (InputHandler::getKeyState(Key::A) == KeyState::Tap ||
+            InputHandler::getKeyState(Key::A) == KeyState::Hold) {
+            pos_.x -= speed * dt; // 왼쪽
+        }
+        if (InputHandler::getKeyState(Key::D) == KeyState::Tap ||
+            InputHandler::getKeyState(Key::D) == KeyState::Hold) {
+            pos_.x += speed * dt; // 오른쪽
+        }
     }
-    if (inputInfo[etoi(Key::Down)].state == KeyState::Tap ||
-        inputInfo[etoi(Key::Down)].state == KeyState::Hold) {
-        dy += speed; // 아래로 이동
-    }
-    if (inputInfo[etoi(Key::Left)].state == KeyState::Tap ||
-        inputInfo[etoi(Key::Left)].state == KeyState::Hold) {
-        dx -= speed; // 왼쪽으로 이동
-    }
-    if (inputInfo[etoi(Key::Right)].state == KeyState::Tap ||
-        inputInfo[etoi(Key::Right)].state == KeyState::Hold) {
-        dx += speed; // 오른쪽으로 이동
-    }
-
-    if (dx != 0.0f || dy != 0.0f) {
-        float length = std::sqrt(dx * dx + dy * dy);
-        if (length > 0) {
-            dx = dx / length * speed * dt;
-            dy = dy / length * speed * dt;
-            pos_.x += dx;
-            pos_.y += dy;
+    else if (pnum_ == 2) 
+        if (InputHandler::getKeyState(Key::I) == KeyState::Tap ||
+            InputHandler::getKeyState(Key::I) == KeyState::Hold) {
+            pos_.y -= speed * dt; // 점프
+        }
+        if (InputHandler::getKeyState(Key::J) == KeyState::Tap ||
+            InputHandler::getKeyState(Key::J) == KeyState::Hold) {
+            pos_.x -= speed * dt; // 왼쪽
+        }
+        if (InputHandler::getKeyState(Key::L) == KeyState::Tap ||
+            InputHandler::getKeyState(Key::L) == KeyState::Hold) {
+            pos_.x += speed * dt; // 오른쪽
         }
     }
 }
